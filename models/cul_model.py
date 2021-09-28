6a004031ecbcf0a14532df732b47cb992e1d7edc
MoonSuin <84334574+MoonSuin@users.noreply.github.com>
2021-09-28 22:23:55 +0900

    update

--- models/cul_model.py
+++ models/cul_model.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 class Culture:
-    def __init__(self,minClassNm=None,svcStateNm=None,svcNm=None,payAtNm=None,placeNm=None,useTgtInfo=None,imgUrl=None, svcUrl=None):
+    def __init__(self,minClassNm=None,svcStateNm=None,svcNm=None,payAtNm=None,placeNm=None,useTgtInfo=None,imgUrl=None, svcUrl=None, x=None, y=None):
         #self.svcid
         self.minClassNm = minClassNm
         self.svcStateNm = svcStateNm
@@ -13,9 +13,9 @@ class Culture:
         self.useTgtInfo = useTgtInfo
         self.imgUrl = imgUrl
         self.svcUrl = svcUrl
+        self.x = x
+        self.y = y
         '''
-        self.x
-        self.y
         self.rcptbgndt
         self.rcptenddt
         self.areanm'''
@@ -50,8 +50,10 @@ class CulService:
                 useTgtInfo = r.find('USETGTINFO').text
                 imgUrl = r.find('IMGURL').text
                 svcUrl = r.find('SVCURL').text
+                x = r.find('X').text
+                y = r.find('Y').text
                 culList.append(Culture(minClassNm=minClassNm, svcStateNm=svcStateNm, svcNm=svcNm, payAtNm=payAtNm,
-                                           placeNm=placeNm, useTgtInfo=useTgtInfo, imgUrl=imgUrl, svcUrl=svcUrl))
+                                           placeNm=placeNm, useTgtInfo=useTgtInfo, imgUrl=imgUrl, svcUrl=svcUrl, x=x, y=y))
             return culList
 
         else:
@@ -73,10 +75,57 @@ class CulService:
                 ongoingList.append(c)
         return ongoingList
 
-    def getByKeyword(self, keyword):
-        culList = self.getList()
+    def getByKeyword(self, culList, keyword):
         result = []
-        for c in culList:
-            if keyword in c.svcNm:
-                result.append(c)
+        if keyword == None:
+            return culList
+        else:
+            for c in culList:
+                if keyword in c.svcNm:
+                    result.append(c)
+            return result
+
+    def getByGeo(self):
+        url = self.base_url + self.api_key + '/xml/ListPublicReservationCulture/1/231/'
+        xml = requests.get(url).text
+        root = BeautifulSoup(xml, 'lxml-xml')
+        code = root.find('CODE').text
+        culList = []
+        if code == 'INFO-000':
+            rows = root.select('row')
+            for r in rows:
+                minClassNm = r.find('MINCLASSNM').text
+                svcStateNm = r.find('SVCSTATNM').text
+                svcNm = r.find('SVCNM').text
+
+                svcNm = svcNm.replace('&#39;', '')
+                svcNm = svcNm.replace('&quot;', '')
+                svcNm = svcNm.replace('&uarr;', '')
+                svcNm = svcNm.replace('&lt;', '')
+                svcNm = svcNm.replace('&gt;', '')
+                svcNm = svcNm.replace('& amp;', '')
+
+                payAtNm = r.find('PAYATNM').text
+                placeNm = r.find('PLACENM').text
+                useTgtInfo = r.find('USETGTINFO').text
+                svcUrl = r.find('SVCURL').text
+                x = r.find('X').text
+                y = r.find('Y').text
+                culList.append(Culture(minClassNm=minClassNm, svcStateNm=svcStateNm, svcNm=svcNm, payAtNm=payAtNm,
+                                       placeNm=placeNm, useTgtInfo=useTgtInfo, svcUrl=svcUrl, x=x, y=y))
+            return culList
+
+        else:
+            print(root.find('RESULT').text)
+
+    def getByCondition(self, condition, keyword):
+        if condition == 'free':
+            culList = self.getFree()
+            result = self.getByKeyword(culList, keyword)
+        elif condition == 'ongoing':
+            culList = self.getOngoing()
+            result = self.getByKeyword(culList, keyword)
+        else:
+            culList = self.getList()
+            result = self.getByKeyword(culList, keyword)
         return result
--- routes/cul_route.py
+++ routes/cul_route.py
@@ -20,8 +20,20 @@ def getOngoing():
     culList = service.getOngoing()
     return render_template('culture/list.html', culList=culList)
 
-@bp.route('/search', methods=['POST'])
+@bp.route('/search1', methods=['POST'])
 def getByKeyword():
     keyword = request.form['keyword']
     culList = service.getByKeyword(keyword)
+    return render_template('culture/list.html', culList=culList)
+
+@bp.route('/map')
+def getMap():
+    culList = service.getByGeo()
+    return render_template('culture/mapK.html', culList=culList)
+
+@bp.route('/search', methods=['POST'])
+def search():
+    condition = request.form['condition']
+    keyword = request.form['keyword']
+    culList = service.getByCondition(condition, keyword)
     return render_template('culture/list.html', culList=culList)
